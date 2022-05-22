6c6693657de668560b97cf7382512f39222b56c4
EvillNeto <evilasio_neto12@hotmail.com>
2022-05-22 19:52:58 -0300

    refatoração no controler

--- src/main/java/dev/evilasio/apitester/controllers/ResponseControler.java
+++ src/main/java/dev/evilasio/apitester/controllers/ResponseControler.java
@@ -1,5 +1,7 @@
 package dev.evilasio.apitester.controllers;
 
+import java.time.Duration;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,7 +23,7 @@ public class ResponseControler {
     ResponseEntity<Mono<Object>> response(@RequestParam(name = "delay", defaultValue = "0") Long delay,
             @RequestParam(name = "response_code", defaultValue = "200") int statusCode) {
 
-        return ResponseEntity.status(statusCode).body(service.objResponse(delay, null));
+        return ResponseEntity.status(statusCode).body(Mono.empty().delaySubscription(Duration.ofSeconds(delay)));
     }
 
     @RequestMapping("/body")
